090555ee5d4783a674c3fc4dd394ae69e34886ad
cookies <in.d2010@yandex.ru>
2022-01-31 20:32:29 +0300

    class constructors

--- src/oop/oop/OOP.java
+++ src/oop/oop/OOP.java
@@ -2,22 +2,23 @@ package oop.oop;
 
 public class OOP {
     public static void main(String[] args) {
-        Transport bmw = new Transport();
-        bmw.setValues(250.5f, 2500, "White", new byte[]{0, 0, 0});
-        String res1 = bmw.getValues();
-        System.out.println(res1);
+        Transport bmw = new Transport(250.5f, 2500, "White", new byte[]{0, 0, 0});
+        //bmw.setValues(250.5f, 2500, "White", new byte[]{0, 0, 0});
+//        String res1 = bmw.getValues();
+//        System.out.println(res1);
 //        bmw.speed = 250.5f;
 //        bmw.weight = 2500;
 //        bmw.color = "White";
 //        bmw.coordinate = new byte[]{0, 0, 0};
 
-        Transport truck = new Transport();
-        truck.speed = 140.5f;
-        truck.weight = 5600;
-        truck.color = "Red";
-        truck.coordinate = new byte[]{100, 0, 100};
-
-        String res2 = truck.getValues();
-        System.out.println(res2);
+        Transport truck = new Transport(5600, new byte[]{100, 0, 100});
+//        Transport truck = new Transport();
+//        truck.speed = 140.5f;
+//        truck.weight = 5600;
+//        truck.color = "Red";
+//        truck.coordinate = new byte[]{100, 0, 100};
+//
+//        String res2 = truck.getValues();
+//        System.out.println(res2);
     }
 }
--- src/oop/oop/Transport.java
+++ src/oop/oop/Transport.java
@@ -6,20 +6,34 @@ public class Transport {
     //protected - поле, класс, метод доступен внутри самого класса и внутри классов наследников
     //private - закрытый, виден в пределах класса
 
-    public float speed;
-    public int weight;
-    public String color;
-    public byte[] coordinate;
+    private float speed;
+    private int weight;
+    private String color;
+    private byte[] coordinate;
 
-    public void setValues(float _speed, int _weight, String _color, byte[] _coordinate) {
-        speed = _speed;
-        weight = _weight;
-        color = _color;
-        coordinate = _coordinate;
+    public Transport(float speed, int weight, String color, byte[] coordinate) {
+        //example Join to DB
+        System.out.println("Object created");
+        setValues(speed, weight, color, coordinate);
+        System.out.println(this.getValues());
+    }
+
+    public Transport(int weight, byte[] coordinate) {
+        this.weight = weight;
+        this.coordinate = coordinate;
+        System.out.println(this.getValues());
+    }
+
+
+    public void setValues(float speed, int weight, String color, byte[] coordinate) {
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
     }
 
     public String getValues() {
-        String info = "Object speed: " + speed + ". Weight: " + weight + ". Color: " + color;
+        String info = "Object speed: " + this.speed + ". Weight: " + this.weight + ". Color: " + this.color;
         String infoCoord = ". \nCoordinates:\n";
         for (int i = 0; i < coordinate.length; i++) {
             infoCoord += coordinate[i] + "\n";
